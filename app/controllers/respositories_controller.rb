fc98b48fcf7fa823a68cf73aaa8dd912c908f29a
Dani Lyn <dlyn1110@gmail.com>
2019-04-03 19:35:46 +0000

    Automatically backed up by Learn

---
+++ app/controllers/respositories_controller.rb
@@ -0,0 +1,23 @@
+class RepositoriesController < ApplicationController
+
+  def search
+
+  end
+
+  def github_search
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+      req.params['client_id'] = ''
+      req.params['client_secret'] = ''
+      req.params['v'] = '20160201'
+      req.params['q'] = params[:query]
+  end
+    body = JSON.parse(@resp.body)
+     if @resp.success?
+      @repo = body['items']
+     else
+      @error = "Sorry, error with @repo."
+     end
+     render 'search'
+   end
+
+  end
